c5dd0d097246f1c3f8ebff33db827740993738b5
Sergio Tabares <alu0101124896@ull.edu.es>
2022-05-05 18:52:24 +0100

    Primera versión de la interfaz para los dummies

---
+++ emisor.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Program to start the execution of the transmitter dummie for the optical
+comunications test and interact with it.
+"""
+
+import os
+import sys
+
+import PySimpleGUI as sg
+
+
+def main():
+    """ Main function to start the execution of the transmitter program. """
+
+    sending_message = False
+    window = define_gui_layout()
+
+    while True:  # Event Loop
+        event, values = window.read(timeout=1)
+        # print(event, values)
+        if event == sg.WIN_CLOSED or event == "-EXIT-":
+            break
+
+        # Folder name was filled in, make a list of files in the folder
+        if event == "-FOLDER-":
+            folder = values["-FOLDER-"]
+            try:
+                file_list = os.listdir(folder)  # get list of files in folder
+            except FileNotFoundError:
+                file_list = []
+            fnames = [
+                f for f in file_list if os.path.isfile(os.path.join(folder, f))
+            ]
+            window["-FILE LIST-"].update(fnames)
+
+        if event == "-SEND-":
+            sending_message = True
+
+        if event == "-STOP-":
+            sending_message = False
+
+        if sending_message:
+            # ToDo: Enviar los datos a la función de encriptado
+
+            params = [
+                values["-PARAM-FREQUENCY-"],
+                values["-PARAM-DUTY_CYCLE-"],
+            ]
+
+            if values['-TOGGLE SEC1-RADIO-']:
+                print(values["-MESSAGE-"], params)
+
+            elif values['-TOGGLE SEC2-RADIO-']:
+                file_path = os.path.join(values['-FOLDER-'],
+                                         values['-FILE LIST-'][0])
+                with open(file_path, "r", encoding='utf-8-sig') as file:
+                    data = file.read()
+                    file.close()
+                    print(data)
+
+            else:
+                sys.exit("Error: Algo raro ha pasao!")
+
+        if event.startswith("-TOGGLE SEC"):
+            window["-SEC1-"].update(visible=values["-TOGGLE SEC1-RADIO-"])
+            window["-SEC2-"].update(visible=values["-TOGGLE SEC2-RADIO-"])
+
+    window.close()
+
+
+def define_gui_layout():
+    """ GUI layout """
+
+    section1 = [
+        [sg.Text("Mensaje:")],
+        [sg.Multiline(size=(50, 10), key="-MESSAGE-")],
+    ]
+
+    section2 = [
+        [
+            sg.Text("Archivo:"),
+            sg.In(size=30, enable_events=True, key="-FOLDER-"),
+            sg.FolderBrowse()
+        ],
+        [
+            sg.Listbox(values=[],
+                       enable_events=True,
+                       size=(50, 10),
+                       key="-FILE LIST-")
+        ],
+    ]
+
+    parameters = [
+        [
+            sg.Text("Frecuencia:"),
+            sg.In(default_text="30", size=5, key="-PARAM-FREQUENCY-"),
+            sg.Text("KHz"),
+        ],
+        [
+            sg.Text("Duty cycle:"),
+            sg.In(default_text="65", size=5, key="-PARAM-DUTY_CYCLE-"),
+            sg.Text("%"),
+        ],
+    ]
+
+    buttons = [[
+        sg.Button("Enviar", key="-SEND-"),
+        sg.Button("Parar", key="-STOP-"),
+        sg.Button("Salir", key="-EXIT-"),
+    ]]
+
+    layout = [
+        [sg.Text("Emisor")],
+        [
+            sg.Radio(" Texto plano",
+                     "Radio",
+                     default=True,
+                     enable_events=True,
+                     key="-TOGGLE SEC1-RADIO-"),
+            sg.Radio(" Archivo",
+                     "Radio",
+                     enable_events=True,
+                     key="-TOGGLE SEC2-RADIO-"),
+        ],
+        [
+            sg.Column(section1, key="-SEC1-"),
+            sg.Column(section2, key="-SEC2-", visible=False),
+        ],
+        [
+            sg.Column(parameters),
+            sg.Column(buttons),
+        ],
+    ]
+
+    window = sg.Window("Emisor", layout)
+    return window
+
+
+if __name__ == "__main__":
+    main()
---
+++ receptor.py
@@ -0,0 +1,126 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Program to start the execution of the receiver dummie for the optical
+comunications test and interact with it.
+"""
+
+import os
+import sys
+
+import PySimpleGUI as sg
+
+
+def main():
+    """ Main function to start the execution of the receiver program. """
+
+    folder = None
+    receiving_message = False
+    window = define_gui_layout()
+
+    while True:  # Event Loop
+        event, values = window.read(timeout=1)
+        # print(event, values)
+        if event == sg.WIN_CLOSED or event == "-EXIT-":
+            break
+
+        # Folder name was filled in, make a list of files in the folder
+        if event == "-FOLDER-":
+            folder = values["-FOLDER-"]
+
+        if event == "-SAVE-":
+            if not os.path.isdir(folder):
+                window["-PATH-ERROR-MSG-"].update(visible=True)
+
+            else:
+                window["-PATH-ERROR-MSG-"].update(visible=False)
+                file_path = os.path.join(folder, values["-FILE_NAME-"])
+
+                # ToDo: Guardar datos al archivo
+                print(file_path)
+
+        if event == "-START-":
+            receiving_message = True
+
+        if event == "-STOP-":
+            receiving_message = False
+
+        if event == "-CLEAN-":
+            window["-MESSAGE-"].update(value='')
+
+        if values['-TOGGLE SEC1-RADIO-'] and receiving_message:
+            # ToDo: Recibir los datos de la función de desencriptado
+
+            window["-MESSAGE-"].update(value=values["-MESSAGE-"] +
+                                       "Recibiendo datos...\n")
+            #! Salto de línea no reconocido
+
+        if event.startswith("-TOGGLE SEC"):
+            window["-SEC1-"].update(visible=values["-TOGGLE SEC1-RADIO-"])
+            window["-SEC2-"].update(visible=values["-TOGGLE SEC2-RADIO-"])
+
+    window.close()
+
+
+def define_gui_layout():
+    """ GUI layout """
+
+    section1 = [
+        [sg.Text("Mensaje:")],
+        [sg.Multiline(size=(50, 10), disabled=True, key="-MESSAGE-")],
+        [
+            sg.Button("Recibir", key="-START-"),
+            sg.Button("Parar", key="-STOP-"),
+            sg.Button("Limpiar", key="-CLEAN-"),
+            sg.Button("Salir", key="-EXIT-"),
+        ],
+    ]
+
+    section2 = [
+        [
+            sg.Text("Archivo:"),
+            sg.In(size=30, enable_events=True, key="-FOLDER-"),
+            sg.FolderBrowse()
+        ],
+        [
+            sg.Text("Nombre de archivo:"),
+            sg.In(default_text='', size=30, key="-FILE_NAME-"),
+        ],
+        [
+            sg.Text("Error: ruta de archivo no válida.",
+                    text_color="red",
+                    background_color="black",
+                    visible=False,
+                    key="-PATH-ERROR-MSG-"),
+        ],
+        [
+            sg.Button("Guardar", key="-SAVE-"),
+            sg.Button("Salir", key="-EXIT-"),
+        ],
+    ]
+
+    layout = [
+        [sg.Text("Receptor")],
+        [
+            sg.Radio(" Mostrar texto",
+                     "Radio",
+                     default=True,
+                     enable_events=True,
+                     key="-TOGGLE SEC1-RADIO-"),
+            sg.Radio(" Guardar en archivo",
+                     "Radio",
+                     enable_events=True,
+                     key="-TOGGLE SEC2-RADIO-"),
+        ],
+        [
+            sg.Column(section1, key="-SEC1-"),
+            sg.Column(section2, key="-SEC2-", visible=False),
+        ],
+    ]
+
+    window = sg.Window("Emisor", layout)
+    return window
+
+
+if __name__ == "__main__":
+    main()
