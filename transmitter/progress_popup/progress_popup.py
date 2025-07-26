dd6f79e4eab9363ceefea6df486c029599a53b53
Sergio Tabares <alu0101124896@ull.edu.es>
2025-07-26 13:08:04 +0100

    Apply Black's auto-formatting

--- transmitter/progress_popup/progress_popup.py
+++ transmitter/progress_popup/progress_popup.py
@@ -6,34 +6,36 @@ from .callbacks import *
 from .progress_data import ProgressData
 
 EVENT_DICTIONARY = {
-  Keys.SKIP: change_to_next_experiment,
+    Keys.SKIP: change_to_next_experiment,
 }
 
+
 def run_progress_window():
-  window = define_gui_layout()
-  data = ProgressData()
-  update_window(window, data)
-  SECONDS_BETWEEN_UPDATES = 3
-  update_timer = Timer(SECONDS_BETWEEN_UPDATES, lambda : 0)
-  update_timer.start()
-  while True:
-    event, values = window.read(timeout=1)
-    if update_timer.finished.is_set():
-      update_window(window, data)
-      update_timer = Timer(SECONDS_BETWEEN_UPDATES, lambda : 0)
-      update_timer.start()
-    if event == sg.TIMEOUT_EVENT:
-      continue
-    if event == sg.WIN_CLOSED or event == Keys.STOP:
-      stop_communication()
-      break
-    if event in EVENT_DICTIONARY:
-      EVENT_DICTIONARY[event](window, values, data)
-    else:
-      sg.popup_error(f"Unknown event ${event}")
-      break
-  update_timer.cancel()
-  window.close()
+    window = define_gui_layout()
+    data = ProgressData()
+    update_window(window, data)
+    SECONDS_BETWEEN_UPDATES = 3
+    update_timer = Timer(SECONDS_BETWEEN_UPDATES, lambda: 0)
+    update_timer.start()
+    while True:
+        event, values = window.read(timeout=1)
+        if update_timer.finished.is_set():
+            update_window(window, data)
+            update_timer = Timer(SECONDS_BETWEEN_UPDATES, lambda: 0)
+            update_timer.start()
+        if event == sg.TIMEOUT_EVENT:
+            continue
+        if event == sg.WIN_CLOSED or event == Keys.STOP:
+            stop_communication()
+            break
+        if event in EVENT_DICTIONARY:
+            EVENT_DICTIONARY[event](window, values, data)
+        else:
+            sg.popup_error(f"Unknown event ${event}")
+            break
+    update_timer.cancel()
+    window.close()
+
 
 if __name__ == "__main__":
-  run_progress_window()
+    run_progress_window()
